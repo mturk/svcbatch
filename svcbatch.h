9db5f2f2f771e3ebb75a9dc969eb20296876b099
Mladen Turk <mturk@apache.org>
2021-02-08 03:41:47 +0100

    Add small comment

--- svcbatch.h
+++ svcbatch.h
@@ -158,6 +158,9 @@
 
 #define SVCBATCH_COMPANY_NAME "Acme Corporation"
 
+#define SVCBATCH_LICENSE_SHORT \
+    "Licensed under the Apache-2.0 License"
+
 #define SVCBATCH_LICENSE \
     "Licensed under the Apache License, Version 2.0 (the ""License""); "        \
     "you may not use this file except in compliance with the License. "         \
