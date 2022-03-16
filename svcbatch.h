69b1cbbfffe3e6e8185c78e1955d03efd5d6f702
Mladen Turk <mturk@apache.org>
2022-03-16 18:51:21 +0100

    Update license string

--- svcbatch.h
+++ svcbatch.h
@@ -209,14 +209,14 @@
     "Licensed under the Apache-2.0 License"
 
 #define SVCBATCH_LICENSE \
-    "Licensed under the Apache License, Version 2.0 (the ""License""); "        \
-    "you may not use this file except in compliance with the License. "         \
-    "You may obtain a copy of the License at\r\n\r\n"                           \
-    "http://www.apache.org/licenses/LICENSE-2.0\r\n\r\n"                        \
-    "Unless required by applicable law or agreed to in writing, software "      \
-    "distributed under the License is distributed on an ""AS IS"" BASIS, "      \
-    "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. " \
-    "See the License for the specific language governing permissions and "      \
+    "Licensed under the Apache License, Version 2.0 (the ""License"");\n"       \
+    "you may not use this file except in compliance with the License.\n"        \
+    "You may obtain a copy of the License at\n\n"                               \
+    "http://www.apache.org/licenses/LICENSE-2.0\n\n"                            \
+    "Unless required by applicable law or agreed to in writing, software\n"     \
+    "distributed under the License is distributed on an ""AS IS"" BASIS,\n"     \
+    "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n"\
+    "See the License for the specific language governing permissions and\n"     \
     "limitations under the License."
 
 
