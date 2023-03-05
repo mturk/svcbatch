1a4e8af0f8a86bca72c71c9a2be50e6a4aefb00a
Mladen Turk <mturk@apache.org>
2023-03-05 15:18:20 +0100

    Use lookup table instead calling wcschr for every character

--- svcbatch.c
+++ svcbatch.c
@@ -134,6 +134,18 @@ static const wchar_t *svcendlogfn = SHUTDOWN_LOGNAME;
 static const wchar_t *localeparam = NULL;
 static const wchar_t *xwoptarg    = NULL;
 
+const char xvalidfname[128] =
+{
+    /** Reject all ctrl codes...                                          */
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    /**   ! " # $ % & ' ( ) * + , - . /  0 1 2 3 4 5 6 7 8 9 : ; < = > ?  */
+        1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,0, 1,1,1,1,1,1,1,1,1,1,0,0,0,1,0,0,
+    /** @ A B C D E F G H I J K L M N O  P Q R S T U V W X Y Z [ \ ] ^ _  */
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1,1,1,1,0,1,0,1,
+    /** ` a b c d e f g h i j k l m n o  p q r s t u v w x y z { | } ~    */
+        0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,0
+};
+
 static const wchar_t *xwcsiid(int i, DWORD c)
 {
     const wchar_t *r;
@@ -1587,7 +1599,7 @@ static DWORD makelogfile(BOOL ssp)
     for (i = 0, x = 0; ewb[i] != WNUL; i++) {
         wchar_t c = ewb[i];
 
-        if (wcschr(L"/:;<>|?*\n\t\"", c) == NULL)
+        if ((c > 127) || xvalidfname[c])
             ewb[x++] = c;
     }
     ewb[x] = WNUL;
